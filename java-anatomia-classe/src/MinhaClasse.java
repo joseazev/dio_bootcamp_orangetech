1f771036f098148e671dffb6786d4dfff98e2259
José Francisco Azevedo da Silva <jose@portoaz.com.br>
2022-12-23 08:57:12 -0300

    Criassão de classe java com minimo necessário para ser chamado de classe.

---
+++ java-anatomia-classe/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+//Classes em java sempre inicião com letras maiusculas
+
+public class MinhaClasse {
+    //sintaxe basica para uma programa ".java" ou classe java.
+    public static void main(String[] args) {
+        System.out.println("Olá turma Boa sejam bem vindos");
+    }
+    
+}
